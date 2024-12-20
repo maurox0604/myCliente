8ea69f4bdfaf295481a852fbf302d54606d085b3
Maurox <xpresar@gmail.com>
2024-12-20 13:04:54 -0500

    Update 20

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -175,4 +175,10 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
     },
+    image: {
+        width: '100%',
+        height: '100%',	
+        resizeMode: 'cover',
+        position: 'absolute',
+    },
 });
