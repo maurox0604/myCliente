ecc29a268616bb32adb48da6d2c7ed723a8fa8bf
Maurox <xpresar@gmail.com>
2025-11-26 08:58:06 -0500

    solucionar deploy babel

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,8 @@
-const { en } = require("react-native-paper-dates");
-
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin','expo-env'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
 
-
-// module.exports = api => {
-//   // Get the platform from the API caller...
-//   const platform = api.caller(caller => caller && caller.platform);
-
-//   return {
-//     presets: ['babel-preset-expo'],
-//     plugins: [
-      
-//       // Add a web-only plugin...
-//       platform === 'web' && 'custom-web-only-plugin',
-//       'react-native-reanimated/plugin',
-//     ].filter(Boolean),
-//   };
-// };
