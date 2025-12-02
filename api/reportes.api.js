840d7dff3d0e7afcd6d6eec3c2c6d2df8e6e5c3a
Maurox <xpresar@gmail.com>
2025-12-02 16:28:27 -0500

    Reportes 1

--- api/reportes.api.js
+++ api/reportes.api.js
@@ -2,3 +2,4 @@ export const getTopSaboresRequest = async () => {
     const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/reportes/top-sabores`);
     return await response.json();
 };
+
